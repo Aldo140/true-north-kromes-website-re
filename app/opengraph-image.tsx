b9981762ddcd9fc0e81a7e7768dce11455183e47
Aldo140 <74526535+Aldo140@users.noreply.github.com>
2026-07-14 12:49:18 -0600

    fix: allow static Pages export

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,7 @@
 import { ImageResponse } from "next/og"
 
+export const dynamic = "force-static"
+
 export const runtime = "edge"
 export const alt = "True North Kromes Inc"
 export const size = { width: 1200, height: 630 }
